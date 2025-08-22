6601dabc2563706488ec2d7152e9fdd01a00763a
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-08-22 18:57:47 +0530

    week 2 progress

--- loops .playground/Contents.swift
+++ loops .playground/Contents.swift
@@ -12,11 +12,19 @@ var tom : Array<Int> = [1,3,9,0,5]
 
 var count = 10
 
+
 while count != 0 {
     print(count)
     count=count-1
 }
+
+
 repeat{
     print("repeat while")
     count=count-1
 }while count == 0
+
+
+for i in stride(from: 0, to: 23, by: 2){
+    print(i)
+}
---
+++ prac/week 2.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+//Find max & min in an array
+//Reverse an array without built-in functions
+//Remove duplicates from an array (using Set)
+//Check if two arrays have common elements (Set intersection)
+//Count frequency of words in a sentence (Dictionary)
+//Two Sum Problem → Find two numbers in an array that add up to a target (Dictionary-based)
+//Flatten a nested array (using recursion + loops)
