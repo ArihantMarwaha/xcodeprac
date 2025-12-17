f4268b662e3d34a7340f8a3787d7805fc665a2be
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-12-17 20:59:52 +0530

    comit

--- DSA main /Palindrome.playground/Contents.swift
+++ DSA main /Palindrome.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+func Palindrome(_ str : String)->Bool{
+    
+    var cleanstring = str.lowercased().filter{$0.isLetter || $0.isNumber}
+    
+    return cleanstring == String(cleanstring.reversed())
+    
+    
+}
+
+Palindrome(greeting)
