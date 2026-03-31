0369fab0076868c5ebb00fa7111d3251d2232542
Arihant Marwaha <arihantmarwaha@icloud.com>
2026-03-31 21:14:49 +0530

    practice prac

--- Testing.playground/Contents.swift
+++ Testing.playground/Contents.swift
@@ -1,3 +1,31 @@
 import UIKit
 
 var greeting = "Hello, playground"
+print(greeting)
+
+
+var s : String = "abc"
+
+func longestSubstring(_ s : String)->Int{
+
+    var char = Array(s)
+    var longest : [Character: Int] = [:]
+    var left = 0
+    var maxlenght : Int = 0
+    
+    for i in 0..<char.count{
+        let c = char[i]
+        
+        if let previous = longest[c],previous >= left{
+            left = previous + 1
+        }
+        longest[c] = i
+        maxlenght = max(i-left+1,maxlenght)
+    }
+    
+    return maxlenght
+}
+
+
+
+print(longestSubstring(s))
